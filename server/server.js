330040e2cafd029611edee1b511cb10675f5d99f
singh <singh1code@gmail.com>
2025-06-10 16:52:22 +0530

    code fixes

--- server/server.js
+++ server/server.js
@@ -2,14 +2,18 @@ const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" }); // Load environment variables first
 
-const DB = process.env.DATABASE_URL;
+// const DB = process.env.DATABASE_URL;
 
 const connectionDb = async () => {
-  return await mongoose.connect(DB, {
+  return await mongoose.connect("mongodb://127.0.0.1:27017/myTesting", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     // useCreateIndex: true, // Deprecated in Mongoose 6+, remove if using newer versions
     // useFindAndModify: false // Deprecated in Mongoose 6+, remove if using newer versions
   });
 };
+
+mongoose.set('bufferTimeoutMS', 30000)
 module.exports = connectionDb;
+// console.log('DB URI:', process.env.DATABASE); // Log to verify
+// console.log('DB Password exists:', !!process.env.DATABASE_PASSWORD); // Log to verify
