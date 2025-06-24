cbb43a572bcaf6372fe4a4b36e859c5b46ed4bef
arjun singh <singh1code@gmail.com>
2025-06-24 12:00:39 +0530

    url updated

--- api/index.js
+++ api/index.js
@@ -18,7 +18,7 @@ app.use((err, req, res, next) => {
 
 
 app.use(cors({
-  origin: ["http://localhost:5173", "https://just-pprlkuwxl-singh-4as-projects.vercel.app/"],
+  origin: ["http://localhost:5173", "https://just-50z8oqssv-singh-4as-projects.vercel.app"],
 }));
 
 
